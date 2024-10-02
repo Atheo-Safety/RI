8ea1413c895df36af3d5021d73e78ae4121cc6f2
Ph3nX-Z <66122220+Ph3nX-Z@users.noreply.github.com>
2024-10-02 11:40:08 +0200

    Update detect_medusa.ps1
    change commandline to work

--- detect_medusa.ps1
+++ detect_medusa.ps1
@@ -102,4 +102,4 @@ function Check-Medusa(){
 # rm NULL
 
 # OneShot:
-# powershell -exec bypass -c (irm -uri https://raw.githubusercontent.com/Atheo-Safety/RI/refs/heads/main/detect_medusa.ps1 > detect_medusa.ps1;ipmo detect_medusa.ps1;Check-Medusa 6> "$(hostname)_medusa.log")
+# powershell -exec bypass -c irm -uri https://raw.githubusercontent.com/Atheo-Safety/RI/refs/heads/main/detect_medusa.ps1 > detect_medusa.ps1;powershell -exec bypass -c 'ipmo detect_medusa.ps1;Check-Medusa 6> "$(hostname)_medusa.log"'
